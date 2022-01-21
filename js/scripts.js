d2f6b328e01afbb977811b10dfd0b8abbe08fd0e
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-01-21 11:28:33 -0800

    WIP - refactoring code

--- js/scripts.js
+++ js/scripts.js
@@ -15,30 +15,45 @@ function robotizeTheNumber (numInput) {
   if ((isNaN(numInput)) || (parseInt(numInput) < 0)) {
     return false; //if not a valid number
   }
-  
-  const robotStrings = ["Beep!","Boop","Won't you be my neighbor?"]
+  // if (numInput === "0") {
+  //   return numInput;
+  // }
+  const robotStrings = ["0","Beep!","Boop","Won't you be my neighbor?"]
   
   for (x = 3; x > 0; x--) {
     if (checkNumberPresence(numInput, x)) {
-      return robotStrings[(x-1)];
+      return robotStrings[(x)];
     }
   }
-  let comboStr = "";
+
+  let numCombo = [];
   for (i = 0; i <= parseInt(numInput); i++) {
-    if (parseInt(numInput) === 0) {
-      comboStr = "0";
-      break;
+    if (robotStrings.indexOf(i)) {
+      numCombo.push(robotStrings[i]);
     } else {
-      if (i !== parseInt(numInput)) {
-        if (i === 1 || i === 2 || i === 3) {
-          comboStr += robotStrings[(i-1)] + ", ";
-        } else {
-          comboStr += i + ", ";
-        }
-      } else {
-          comboStr += i;
-        }
-      }
+      numCombo.push(i);
+    }
   }
-  return comboStr;
+  return numCombo;
 }
+
+
+
+// let comboStr = "";
+// for (i = 0; i <= parseInt(numInput); i++) {
+//   if (parseInt(numInput) === 0) {
+//     comboStr = "0";
+//     break;
+//   } else {
+//     if (i !== parseInt(numInput)) {
+//       if (i === 1 || i === 2 || i === 3) {
+//         comboStr += robotStrings[(i-1)] + ", ";
+//       } else {
+//         comboStr += i + ", ";
+//       }
+//     } else {
+//         comboStr += i;
+//       }
+//     }
+// }
+// return comboStr;
