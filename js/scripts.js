b061b33074384fe29a46ac9cab5c7c2a4c831117
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-01-21 12:11:15 -0800

    build out basic interface with input and output

--- js/scripts.js
+++ js/scripts.js
@@ -13,22 +13,21 @@ function checkNumberPresence (fullNumber, numToFind) {
 function robotizeTheNumber (numInput) {
 
   if ((isNaN(numInput)) || (parseInt(numInput) < 0)) {
-    return false; //if not a valid number
+    return false; //if not a valid number, or if num is less than 0
   }
-  // if (numInput === "0") {
-  //   return numInput;
-  // }
-  const robotStrings = ["0","Beep!","Boop","Won't you be my neighbor?"]
+  const robotStrings = ["0","Beep!","Boop","Won't you be my neighbor?"];
   
   for (x = 3; x > 0; x--) {
     if (checkNumberPresence(numInput, x)) {
-      return robotStrings[(x)];
+      return robotStrings[(x)]; //returns the corresponding string in robotStrings
     }
   }
 
+  //if no 3, 2, or 1 is present, the below runs. It counts up from 0 to the number input, replacing 0, 1, 2, and 3 as it
+  //counts with the corresponding string for each number from robotStrings[];
   let numCombo = [];
   for (i = 0; i <= parseInt(numInput); i++) {
-    if (robotStrings.indexOf(i)) {
+    if (robotStrings[i]) {
       numCombo.push(robotStrings[i]);
     } else {
       numCombo.push(i);
@@ -37,23 +36,13 @@ function robotizeTheNumber (numInput) {
   return numCombo;
 }
 
+//UI Logic
 
+$(document).ready(function() {
+	$("form#ask-the-robot").submit(function(event) {
+    event.preventDefault();
+    $("#robot-paragraph").text(robotizeTheNumber($("#user-input").val()));
+    $("#user-input").val("");
+})
 
-// let comboStr = "";
-// for (i = 0; i <= parseInt(numInput); i++) {
-//   if (parseInt(numInput) === 0) {
-//     comboStr = "0";
-//     break;
-//   } else {
-//     if (i !== parseInt(numInput)) {
-//       if (i === 1 || i === 2 || i === 3) {
-//         comboStr += robotStrings[(i-1)] + ", ";
-//       } else {
-//         comboStr += i + ", ";
-//       }
-//     } else {
-//         comboStr += i;
-//       }
-//     }
-// }
-// return comboStr;
+});
